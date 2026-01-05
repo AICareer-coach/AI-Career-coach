cad9b63bab6ff247f67889f6570757b270fbab0e
AICareer-coach <aicareer123@gmail.com>
2026-01-05 20:35:08 +0530

    Update auth.js

--- docs/script/auth.js
+++ docs/script/auth.js
@@ -32,16 +32,12 @@ auth.onAuthStateChanged(user => {
         // console.log("Current window.location.pathname:", window.location.pathname);
         // If not logged in and on a protected page, redirect to index.html
         // (index.html will then guide them to login/signup)
-        const protectedPaths = ['/AI-Career-coach/home.html', '/AI-Career-coach/profile.html', '/AI-Career-coach/optimizer.html', '/AI-Career-coach/roadmap.html', '/AI-Career-coach/joblisting.html','/AI-Career-coach/assessment.html','/AI-Career-coach/interview.html'];
+        const protectedPaths = ['/home.html', '/profile.html', '/optimizer.html', '/roadmap.html', '/joblisting.html','/assessment.html','/interview.html'];
         if (protectedPaths.includes(window.location.pathname) || (window.location.pathname.startsWith('/script') && !window.location.pathname.includes('login.js'))) {
             // console.log("Redirecting to index.html from a protected path.");
-            window.location.href = '/AI-Career-coach/index.html';
+            window.location.href = '/index.html';
         }
         // If already on index.html or login.html, do nothing (let them choose)
     }
 });
-
-
-
-
 
