e84cf596f4de69dfebb9390cd232b3eba9e63cf0
AICareer-coach <aicareer123@gmail.com>
2026-01-12 22:12:16 +0530

    Update auth.js

--- docs/script/auth.js
+++ docs/script/auth.js
@@ -1,18 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDtuYr4icwQf2HsvByrCZeqbEex28lL6GI", // <--- IMPORTANT: Replace with your actual Firebase API key
-    authDomain: "genaihack-240d7.firebaseapp.com",
-    projectId: "genaihack-240d7",
-    storageBucket: "genaihack-240d7.firebasestorage.app",
-    messagingSenderId: "1095624251792",
-    appId: "1:1095624251792:web:8b4be21e68c1a8bcc2bb15"
-};
-
-// Initialize Firebase if it hasn't been already
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-const auth = firebase.auth();
-
 // This is the core of the security guard.
 // It listens for changes in the user's authentication state.
 auth.onAuthStateChanged(user => {
@@ -40,5 +25,6 @@ auth.onAuthStateChanged(user => {
         // If already on index.html or login.html, do nothing (let them choose)
     }
 });
+
 
 
